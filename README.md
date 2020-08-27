5aba0526d0a115ee15eb7a2740b0b84aea618cca
dirgayeah23 <rezadirgantara91@gmail.com>
2020-08-27 12:33:30 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h2>Simple Email Using Python Scrip</h2>
+
+<p>Python version 3.8.3</p> 
---
+++ send_email.py
@@ -0,0 +1,17 @@
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = input ("masukkan email pengirim :")  # Masukkan email anda
+receiver_email = input ("masukkan email penerima :")  # masukkan email penerima
+password = input("Masukkan Password dan tekan enter: ")
+message = """\
+Subject: Halo 
+
+Pesan ini di kirim melalui python script."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
+    print('Email terkirim')
